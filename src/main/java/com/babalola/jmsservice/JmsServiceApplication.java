643f3e8e0426bbcc32f274b7e049be8219a8267b
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2022-12-13 20:54:38 +0100

    Added configuration for activeMQ broker to send and receive messages

--- src/main/java/com/babalola/jmsservice/JmsServiceApplication.java
+++ src/main/java/com/babalola/jmsservice/JmsServiceApplication.java
@@ -1,9 +1,5 @@
 package com.babalola.jmsservice;
 
-import org.apache.activemq.artemis.core.config.Configuration;
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JmsServiceApplication {
 
     public static void main(String[] args) throws Exception {
-        ActiveMQServer newServer = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0"));
-
-        newServer.start();
+//        ActiveMQServer newServer = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//                .setPersistenceEnabled(false)
+//                .setJournalDirectory("target/data/journal")
+//                .setSecurityEnabled(false)
+//                .addAcceptorConfiguration("invm", "vm://0"));
+//
+//        newServer.start();
 
         SpringApplication.run(JmsServiceApplication.class, args);
 
